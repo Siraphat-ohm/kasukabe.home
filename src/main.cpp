cf9c051f9186e0a3eeb3098ec67a503250892304
lawiet <siraphat975@gmail.com>
2023-02-08 12:02:11 -0500

    implement ultrasonic

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <Ultrasonic.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+Ultrasonic ultrasonic(D7, D8);
 
 void setup() {
   lcd.init();
@@ -11,6 +13,12 @@ void setup() {
 }
 
 void loop() {
+  int distance = ultrasonic.read();
+
   lcd.setCursor(0,0);
-  lcd.print("hello bua");
+  lcd.print("distance : ");
+  lcd.print(distance);
+  lcd.print("cm");
+  delay(1000);
+  lcd.clear();
 }
