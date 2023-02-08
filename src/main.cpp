faf0c111fb0cda42c68ecc000c800d6b91e8fdaf
lawiet <siraphat975@gmail.com>
2023-02-08 11:36:34 -0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  Serial.begin(9600);
+}
+
+void loop() {
+  lcd.setCursor(0,0);
+  lcd.print("hello bua");
+}
